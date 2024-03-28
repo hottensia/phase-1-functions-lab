9a51d5fbe9e8b6ef8afb9e416c1b8ef6887944c7
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-03-28 16:52:45 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    let hq = 42
+    if (hq <= pickup) {
+        return pickup - hq
+    }
+    else return hq - pickup
+}
+function distanceFromHqInFeet(pickup) {
+    let hq = 42
+    if (hq <= pickup) {
+        return (pickup - hq) * 264
+    }
+    else return (hq - pickup) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination)
+        return (destination - start) * 264
+    else return (start - destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+    let dist = distanceTravelledInFeet(start, destination)
+    if (dist <= 400)
+        return (0)
+    else if (dist > 400 && dist <= 2000)
+        return ((dist - 400) * 0.02)
+    else if (dist > 2000 && dist <= 2500)
+        return (25)
+    else  (dist >2500)
+    return ('cannot travel that far')
+}
